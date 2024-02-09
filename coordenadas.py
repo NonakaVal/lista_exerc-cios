c25eaa0a8443551a9567c110ef5fc96b5a1ddab7
Valdenir Nonaka <nonaka96@outlook.com>
2024-02-09 13:14:06 -0300

    09/02/24

--- coordenadas.py
+++ coordenadas.py
@@ -1,13 +1,15 @@
 
-
-
 def movimento(mensagem):
+    """Movimenta a posição no plano cartesiano, começando no valor 0,0
+    x, y: define a posição
+    directions: define com base no texto informado quais números serão alterados
+    choice: permite que o usuário escolha a direção em um loop que verifica se a entrada é válida
+    """
     x, y = 0, 0
     directions = {'cima': (0, 1), 'baixo': (0, -1), 'direita': (1, 0), 'esquerda': (-1, 0)}
 
     while True:
         choice = input(mensagem)
-
         if choice in directions:
             dx, dy = directions[choice]
             x += dx
@@ -16,7 +18,7 @@ def movimento(mensagem):
         else:
             print("Escolha inválida. Use 'cima', 'baixo', 'direita' ou 'esquerda'.")
 
-# Exemplo de uso
+
 movimento("Escolha a direção ('cima', 'baixo', 'direita' ou 'esquerda'): ")
 
 
